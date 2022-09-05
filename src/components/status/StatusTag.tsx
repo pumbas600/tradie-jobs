6a3b7cc98cf13c61feff36fecb0dffc193d8b44d
pumbas600 <joshjeffers600@gmail.com>
2022-09-05 19:33:12 +1200

    feat: Create status component

---
+++ src/components/status/StatusTag.tsx
@@ -0,0 +1,22 @@
+import { Stack, Text } from '@chakra-ui/react';
+import { Status } from '../../types/JobInfo';
+
+const StatusTag = ({ status }: { status: Status }) => {
+    const colours: Record<Status, string> = {
+        [Status.Active]: 'green.500',
+        [Status.Completed]: 'blue.600',
+        [Status.Invoicing]: 'blue.400',
+        [Status.Scheduled]: 'orange.400',
+        [Status.ToPrice]: 'orange.600',
+    };
+
+    return (
+        <Stack w="120px" borderRadius="lg" bg={colours[status]} px={4} py={0.5} gap={2}>
+            <Text color="white" fontWeight="semibold" whiteSpace="nowrap">
+                {status}
+            </Text>
+        </Stack>
+    );
+};
+
+export default StatusTag;
